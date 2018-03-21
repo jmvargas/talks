b7beb34f71d7f88f43be7c24eded40b892efa152
Jesús Manuel Vargas <estudiantejm@gmail.com>
2018-03-21 20:28:39 +0100

    Updated an example

--- 2018/go-by-example/concurrency/103.go
+++ 2018/go-by-example/concurrency/103.go
@@ -10,8 +10,8 @@ func worker(id int, jobs <-chan int, results chan<- int) {
 	for job := range jobs {
 		fmt.Println("worker", id, "started  job", job)
 		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", job)
 		results <- job * 2
+		fmt.Println("worker", id, "finished job", job)
 	}
 }
 
